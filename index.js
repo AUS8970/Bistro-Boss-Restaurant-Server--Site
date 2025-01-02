fde3290d7c270bb5f7b2a022192a5c6441f9e7e5
aus8970 <www.aus8970@gmail.com>
2025-01-02 23:30:37 +0600

    module 65 end

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yn4cz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const menuCollection = client.db("bistroDB").collection("menu");
+    const reviewsCollection = client.db("bistroDB").collection("reviews");
+
+    app.get('/menu', async(req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+    
+    app.get('/reviews', async(req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+  } finally {}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Welcome To You in Our Bistro Boss Server!')

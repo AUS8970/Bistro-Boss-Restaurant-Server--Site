a65e16a15d9e9a759bb2c9cf7c850a07c8bf2d7a
aus8970 <www.aus8970@gmail.com>
2025-01-06 20:53:41 +0600

    module 69

--- index.js
+++ index.js
@@ -48,9 +48,8 @@ async function run() {
       res.send({ token });
     })
 
-    // middlewares
+    // verify token middlewares
     const verifyToken = (req, res, next) => {
-      console.log('verify token', req.headers.authorization);
       if(!req.headers.authorization){
         return res.status(401).send({message: 'Unauthorized Access'})
       }
@@ -64,11 +63,12 @@ async function run() {
       });
     };
 
+    // verify admin middlewares
     const verifyAdmin = async(req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === 'admim';
+      const isAdmin = user?.role === 'admin';
       if(!isAdmin){
         return res.status(403).send({message: "Forbidden Access"})
       }
@@ -81,7 +81,7 @@ async function run() {
       res.send(result)
     });
 
-    app.get('user/admin/:email', verifyToken, async(req, res) => {
+    app.get('/user/admin/:email', verifyToken, async(req, res) => {
       const email = req.params.email;
       if (email !== req.decoded.email) {
         return res.status(403).send({message: "Forbidden Access"})
@@ -107,10 +107,10 @@ async function run() {
       res.send(result)
     });
 
-    app.delete('/users/:id', async(req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
-      const result = await userCollection.insertOne(query);
+      const result = await userCollection.deleteOne(query);
       res.send(result)
     });
 
@@ -130,6 +130,43 @@ async function run() {
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/menu', verifyToken, verifyAdmin, async(req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result)
+    });
+
+    app.patch('/menu/:id', async(req, res) => {
+      const item =req.body;
+      const id = req.params.id;
+      const filter = { _id: id}
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          categroy: item.categroy,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        }
+      }
+      const result  =await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result)
     });
     
     // get reviews data from mongodb reviews collection
@@ -152,7 +189,7 @@ async function run() {
       res.send(result)
     });
 
-    app.delete('/carts/:id', verifyToken, verifyAdmin, async(req, res) => {
+    app.delete('/carts/:id', verifyToken, async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await cartCollection.deleteOne(query);

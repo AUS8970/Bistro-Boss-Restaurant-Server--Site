42a6e1ac6a6594c33825c7fc24d6e670920e35e2
aus8970 <www.aus8970@gmail.com>
2025-01-10 23:43:53 +0600

    module 71

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ async function run() {
       }
       const result = await paymentCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     // payment related api
     app.post('/payments', async(req, res) => {
@@ -251,6 +251,73 @@ async function run() {
 
       // send response
       res.send({paymentResult, deleteResult})
+    });
+
+    // stats or analytics
+    app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      const result = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {
+              $sum: '$price'
+            }
+          }
+        }
+      ]).toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0 ;
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue
+      })
+    });
+
+    app.get('/order-stats', async(req, res) => {
+      const result = await paymentCollection.aggregate([
+        {
+          $unwind: '$menuItemIds'
+        },
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItemIds',
+            foreignField: '_id',
+            as: 'menuItems'
+          }
+        },
+        {
+          $unwind: '$menuItems'
+        },
+        {
+          $group: {
+            _id: '$menuItems.category',
+            quantity: {
+              $sum: 1,
+            },
+            revenue: {
+              $sum: '$menuItems.price'
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            category: '$_id',
+            quantity: '$quantity',
+            revenue: '$revenue'
+          }
+        }
+      ]).toArray();
+
+      res.send(result);
     })
 
   } finally {}
